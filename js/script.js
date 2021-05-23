06f6218b6bc01fdbebb74a8c7341c4fdc570aa18
Anna FitzGerald <46199551+AEFitzGerald@users.noreply.github.com>
2021-05-23 09:04:00 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//change background color of footer
+var footer = document.querySelector(".contact");
+footer.style.backgroundColor = "#d7ddf3";
+
+//change slogan
+var tagline = document.querySelector("h2");
+tagline.innerText = "Don't be a prick, let us prick your roses!";
+
+//change line height and fontsize of address
+var addressElement = document.querySelector("address");
+addressElement.style.lineHeight = "1.5em";
+addressElement.style.fontSize = "1.75em";
+
+//change emphasis in contact section
+var contactSlogan = document.querySelector("h3");
+contactSlogan.innerHTML = "We are here for you <em>everyday</em> of the week";
